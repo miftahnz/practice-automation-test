b15870f74d6292dc8384a0568ec0009f7a69bf11
miftahnz <miftah.nurzanah30@gmail.com>
2025-02-05 14:51:36 +0700

    update valid password and add testcase

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,13 +3,14 @@ import { LoginPage } from '../pages/LoginPage';
 
 // Constants
 const VALID_USERNAME = 'student';
-const VALID_PASSWORD = 'password123';
+const VALID_PASSWORD = 'Password123';
 const INVALID_USERNAME = 'wrongUsername';
 const INVALID_PASSWORD = 'invalidPassword';
 const ERROR_INVALID_USERNAME = 'Your username is invalid!';
 const ERROR_INVALID_PASSWORD = 'Your password is invalid!';
 const SUCCESS_MESSAGE = 'Congratulations student. You successfully logged in!';
 const SUCCESS_PAGE_TITLE = 'Logged In Successfully';
+const EMPTY_STRING = '';
 
 test.beforeEach(async ({ page }) => {
   const loginPage = new LoginPage(page);
@@ -38,5 +39,26 @@ test('User Failed Login with Invalid Password', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   await loginPage.login(VALID_USERNAME, INVALID_PASSWORD);
+  await expect(loginPage.errorSnackbar).toContainText(ERROR_INVALID_PASSWORD);
+});
+
+test('User Failed Login when Username and Password Empty', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.login(EMPTY_STRING, EMPTY_STRING);
+  await expect(loginPage.errorSnackbar).toContainText(ERROR_INVALID_USERNAME);
+});
+
+test('User Failed Login when Username Empty', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.login(EMPTY_STRING, INVALID_PASSWORD);
+  await expect(loginPage.errorSnackbar).toContainText(ERROR_INVALID_USERNAME);
+});
+
+test('User Failed Login when Password Empty', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.login(VALID_USERNAME, EMPTY_STRING);
   await expect(loginPage.errorSnackbar).toContainText(ERROR_INVALID_PASSWORD);
 });
